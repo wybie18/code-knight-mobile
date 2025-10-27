c68248c2028b7c082267e25260f9804397a4ee75
Wybie <lyrncofyn@gmail.com>
2025-10-28 02:52:24 +0800

    feat(layout): implement initial layout with authentication and navigation bar setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,43 @@
+import { AuthProvider } from "@/context/AuthContext";
+import { useAuth } from "@/hooks/useAuth";
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
+import * as NavigationBar from "expo-navigation-bar";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { Platform } from "react-native";
+import "../styles/global.css";
+
+const InitialLayout = () => {
+  const {isAuthenticated} = useAuth();
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Protected guard={isAuthenticated}>
+        <Stack.Screen name="(tabs)"/>
+      </Stack.Protected>
+
+      <Stack.Protected guard={!isAuthenticated}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="register" />
+      </Stack.Protected>
+      <Stack.Screen name="loading"/>
+    </Stack>
+  );
+};
 
 export default function RootLayout() {
-  return <Stack />;
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      NavigationBar.setBackgroundColorAsync("#000000");
+      NavigationBar.setButtonStyleAsync("light");
+    }
+  }, []);
+  return (
+    <ThemeProvider value={DarkTheme}>
+      <AuthProvider>
+        <InitialLayout />
+      </AuthProvider>
+      <StatusBar style="light" />
+    </ThemeProvider>
+  );
 }
