e9457d553a9f70441c9458ab4201869ebf7a097e
Wybie <lyrncofyn@gmail.com>
2025-10-30 17:04:14 +0800

    fix(api): update Axios baseURL to use environment variable for better configuration

--- api/AxiosConfig.ts
+++ api/AxiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://192.168.1.9:8000/api',
+  baseURL: process.env.EXPO_PUBLIC_API_URL || 'http://localhost:8000/api',
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
