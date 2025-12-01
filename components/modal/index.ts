564d932c21c420dc8dd98a499e67d70241113e33
Wybie <lyrncofyn@gmail.com>
2025-12-02 00:54:47 +0800

    feat(modal): export CtfChallengeModal from modal index

--- components/modal/index.ts
+++ components/modal/index.ts
@@ -1,3 +1,4 @@
+export { default as CtfChallengeModal } from "./CtfChallengeModal";
 export { default as FilterModal } from "./FilterModal";
 export { default as FilterSection } from "./FilterSection";
 
